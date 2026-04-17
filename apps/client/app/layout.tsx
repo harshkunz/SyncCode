665bfdb712f224aebc4956c2e6b22c31b04b11e5
harshkunz <harshkunz0@gmial.com>
2026-04-17 07:17:15 +0530

    fix: next config

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -12,25 +12,85 @@
 import type { Metadata, Viewport } from 'next';
 
 import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
 
+import { NAME, PORTFOLIO_URL, SITE_DESCRIPTION, SITE_NAME } from '@/lib/constants';
+import { Toaster } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { Analytics } from '@/components/shared/analytics';
+import { ThemeProvider } from '@/components/shared/theme-provider';
 
-import './globals.css';
+import './global.css';
 
 // export const runtime = 'edge';
 
 const DEFAULT_URL = process.env.NEXT_PUBLIC_CLIENT_URL || 'https://synccode.vercel.app';
 
-export const metadata: Metadata = {}
+export const metadata: Metadata = {
+  title: {
+    default: SITE_NAME,
+    template: `%s | ${SITE_NAME}`
+  },
+  description: SITE_DESCRIPTION,
+  applicationName: SITE_NAME,
+  referrer: 'origin-when-cross-origin',
+  keywords:
+    'Synccode, code collaboration, real-time coding, pair programming, remote collaboration, live coding, code sharing, collaborative editor, monaco editor, cursor sharing, live preview, video chat, collaborative terminal, shared terminal, code execution, GitHub integration, web IDE, online IDE, collaborative development, coding platform, programming tools',
+  creator: NAME,
+  publisher: NAME,
+  authors: {
+    name: NAME,
+    url: PORTFOLIO_URL
+  },
+  metadataBase: new URL(DEFAULT_URL),
+  formatDetection: {
+    telephone: false
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default'
+  },
+  openGraph: {
+    type: 'website',
+    siteName: SITE_NAME,
+    locale: 'en_US',
+    url: DEFAULT_URL
+  },
+  alternates: {
+    canonical: DEFAULT_URL
+  }
+};
 
-export const viewport: Viewport = {}
+export const viewport: Viewport = {
+  themeColor: '#0a0a0a',
+  width: 'device-width',
+  initialScale: 1,
+  userScalable: false,
+  maximumScale: 1,
+  viewportFit: 'cover'
+};
 
-export default function RootLayout({}) {
+export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html>
+    <html
+      lang="en"
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
+      suppressHydrationWarning
+    >
       <head>
-        <meta name="" />
+        <meta name="darkreader-lock" />
       </head>
-      <body className="">
+      <body className="h-dvh text-pretty antialiased">
+        <Analytics />
+        <ThemeProvider attribute="class" disableTransitionOnChange>
+          <TooltipProvider>{children}</TooltipProvider>
+          <Toaster
+            richColors
+            className="whitespace-pre-line"
+            pauseWhenPageIsHidden
+            containerAriaLabel="Toast Notifications"
+          />
+        </ThemeProvider>
       </body>
     </html>
   );
